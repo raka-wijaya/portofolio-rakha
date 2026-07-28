9b4b45e95e28913dad70d234b5dac12653b07dd6
salendra rakha wijaya <salendraraka7@gmail.com>
2026-07-28 09:47:42 +0700

    fix: Add text-justify class to Tools description paragraph
    Updated text-justify class for paragraph in Tools component.

--- src/pages/Tools.jsx
+++ src/pages/Tools.jsx
@@ -10,7 +10,7 @@ function Tools() {
     <section className='container mx-auto py-6 px-4 md:py-6 md:px-12 mt-5'>
       <div className='text-center gap-10'>
         <h2 data-aos="fade-up" data-aos-delay="50" className='text-3xl font-bold font-Poppins text-gray-900 mb-2'>{t("tools.title")} &amp; {t("tools.title_1")}</h2>
-        <p data-aos="fade-up" data-aos-delay="100" className='text-gray-600 max-w-2xl mx-auto'>{t("tools.desc")}</p>
+        <p data-aos="fade-up" data-aos-delay="100" className='text-gray-600 max-w-2xl mx-auto text-justify'>{t("tools.desc")}</p>
       </div>
       
       <div className='flex flex-wrap justify-center gap-5 mt-7'>
