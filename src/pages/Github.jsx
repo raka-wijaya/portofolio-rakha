cddfb089a0c466f594f92b0c5be47b50f2ff9a96
salendra rakha wijaya <salendraraka7@gmail.com>
2026-07-28 09:50:01 +0700

    fix: Justify the description paragraph in GithubChart
    Updated the description paragraph to be justified.

--- src/pages/Github.jsx
+++ src/pages/Github.jsx
@@ -20,7 +20,7 @@ function GithubChart() {
     return (
         <div className="mx-auto flex flex-col justify-center items-center">
             <h1 data-aos="fade-up" data-aos-delay="50" className="text-3xl font-bold font-Poppins text-gray-900 mb-2">{t("github.title")}</h1>
-            <p data-aos="fade-up" data-aos-delay="100" className="text-gray-500 mb-2 font-Poppins">{t("github.desc")}</p>
+            <p data-aos="fade-up" data-aos-delay="100" className="text-gray-500 mb-2 font-Poppins text-justify">{t("github.desc")}</p>
             {joinDate && (
                 <p data-aos="fade-up" data-aos-delay="150" className="text-sm text-blue-600 font-Poppins mb-4">
                     github since {joinDate}
