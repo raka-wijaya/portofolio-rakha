dea3f4648cdb0771c9140b979e03fe100cd493a6
salendra rakha wijaya <salendraraka7@gmail.com>
2026-07-30 20:28:04 +0700

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://zvwksmipfjjdsnmjkhna.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp2d2tzbWlwZmpqZHNubWpraG5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4OTc2OTEsImV4cCI6MjA4OTQ3MzY5MX0.9cm0LDSEg-ryd-fIg9sEPcpfw37NdcajSoVuGIkhhY8'
 
-=======
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://zvwksmipfjjdsnmjkhna.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp2d2tzbWlwZmpqZHNubWpraG5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4OTc2OTEsImV4cCI6MjA4OTQ3MzY5MX0.9cm0LDSEg-ryd-fIg9sEPcpfw37NdcajSoVuGIkhhY8'
-
->>>>>>> master
 export const supabase = createClient(supabaseUrl, supabaseKey)
